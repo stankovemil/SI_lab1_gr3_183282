bd49f931d88d984115e19af861f3854b438d98a8
stankovemil <emilstankov55@gmail.com>
2020-03-24 17:23:04 +0100

    tax type variable added

--- Item.java
+++ Item.java
@@ -2,6 +2,46 @@ class Item {
 	int id;
 	String name;
 	double price;
+	char taxType;
+
+	public Item(int id, String name, double price, char taxType) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.taxType = taxType;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public char getTaxType() {
+		return taxType;
+	}
+
+	public void setTaxType(char taxType) {
+		this.taxType = taxType;
+	}
 
 	//TODO add variable.
 
@@ -9,7 +49,8 @@ class Item {
 
 	//TODO setters and getters
 
+
 	double taxReturn () {
 		//TODO
-	}
+
 }
